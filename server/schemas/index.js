e5dd47712b48b295d53e4ab7ed6219be2a6d9b67
zaort <czafra.ortega@gmail.com>
2024-03-21 22:14:15 -0600

    Initial structure with adjusted server for Apollo Server / graphQL

---
+++ server/schemas/index.js
@@ -0,0 +1,17 @@
+const typeDefs = require("./typeDefs");
+const resolvers = require("./resolvers");
+
+module.exports = { typeDefs, resolvers };
+
+// const router = require('express').Router();
+// const path = require('path');
+// const apiRoutes = require('./api');
+
+// router.use('/api', apiRoutes);
+
+// // serve up react front-end in production
+// router.use((req, res) => {
+//   res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+// });
+
+// module.exports = router;
---
+++ server/schemas/resolvers.js
@@ -0,0 +1,71 @@
+const { User } = require("../models");
+const { signToken, AuthenticationError } = require("../utils/auth");
+
+const resolvers = {
+ Query: {
+  me: async (parent, args, context) => {
+   if (context.user) {
+    const userData = await User.findOne({ _id: context.user._id }).select("-__v -password");
+    return userData;
+   }
+   return null;
+  },
+ },
+ Mutation: {
+  login: async (parent, { email, password }) => {
+   try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+     throw AuthenticationError;
+    }
+
+    const correctPw = await user.isCorrectPassword(password);
+
+    if (!correctPw) {
+     throw AuthenticationError;
+    }
+    // console.log(user);
+    const token = signToken(user);
+    // console.log(token);
+    return { token, user };
+   } catch (err) {
+    console.log(err);
+    return err;
+   }
+  },
+  createUser: async (parent, { username, email, password }) => {
+   try {
+    const user = await User.create({ username, email, password });
+    const token = signToken(user);
+    return { token, user };
+   } catch (err) {
+    console.log(err);
+   }
+  },
+  saveBook: async (parent, { bookData }, context) => {
+   if (context.user) {
+    const updatedUser = await User.findByIdAndUpdate(
+     { _id: context.user._id },
+     { $push: { savedBooks: bookData } },
+     { new: true }
+    );
+    return updatedUser;
+   }
+   throw new Error("You need to be logged in!");
+  },
+  removeBook: async (parent, { bookId }, context) => {
+   if (context.user) {
+    const updatedUser = await User.findByIdAndUpdate(
+     { _id: context.user._id },
+     { $pull: { savedBooks: { bookId } } },
+     { new: true }
+    );
+    return updatedUser;
+   }
+   throw new Error("You need to be logged in!");
+  },
+ },
+};
+
+module.exports = resolvers;
